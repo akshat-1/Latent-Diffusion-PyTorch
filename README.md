d4cd8d05cda23c77e212c9cf40d05a629fe0c62a
akshat-1 <mpakshat@gmail.com>
2025-08-09 16:12:31 +0530

    Model developed

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Latent-Diffusion-PyTorch
+Latent Diffusion model is a Diffusion model which works in Latent Space.
+## Table of Contents
+
+- [Model Architecture](#model-architecture)
+- [Training Process](#training-process)
+- [Model Evaluation](#model-evaluation)
+- [Usage](#usage)
+- [Dependencies](#dependencies)
+- [References](#references)
+- [Improvements](#improvements)
+
+
+## Model Arcihtecture
+1. Encoder & Decoder (VAE)
+
---
+++ src/run.py
@@ -0,0 +1,63 @@
+import model_loader
+import pipeline
+import cv2
+from transformers import CLIPTokenizer
+import torch
+
+DEVICE= "cpu"
+
+ALLOW_CUDA = True
+ALLOW_MPS= True
+
+if torch.cuda.is_available() and ALLOW_CUDA:
+    DEVICE = "cuda"
+elif (torch.has_mps or torch.backends.mps.is_available()) and ALLOW_MPS:
+    DEVICE= "mps"
+
+print(f"Using device {DEVICE}")
+
+tokenizer = CLIPTokenizer("data/vocab.json" , merges_file = "data/merges.txt")
+model_file = "data/v1-5-pruned-emaonly.ckpt"
+models = model_loader.perload_models_from_standard_weights(model_file, DEVICE)
+
+prompt = "Generate an image with text - Envisage 14.0 Summer Training in white background, with lots of designs in 4k"
+
+uncond_prompt = "Dark background"  
+do_cfg = True
+cfg_scale =7
+
+
+#I - I 
+
+input_image = None
+image_path = "images/temp.png"
+if(input_image): 
+    input_image = cv2.imread(image_path, cv2.IMREAD_COLOR)
+    
+
+strength = 0.9
+sampler ="ddpm"
+num_inference_steps = 100
+seed =42
+
+output_image = pipeline.generate(
+    prompt = prompt,
+    uncond_prompt= uncond_prompt,
+    input_image=input_image,
+    strength= strength,
+    do_cfg = do_cfg,
+    cfg_scale = cfg_scale,
+    sampler_name = sampler,
+    n_inference_steps = num_inference_steps,
+    seed =seed,
+    models = models,
+    device = DEVICE,
+    idle_device = "cpu",
+    tokenizer = tokenizer
+
+
+)
+cv2.imwrite('output_image.jpg', output_image)
+cv2.imshow('Image', output_image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
